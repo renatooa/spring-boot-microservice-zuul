7fcb3930ba434d954e7f9481f2459b6105851e5e
Renato Alves <renatoalves.infor@gmail.com>
2021-04-20 09:42:46 -0300

    vercionando o projeto

---
+++ src/test/java/br/com/renato/config/server/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renato.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
